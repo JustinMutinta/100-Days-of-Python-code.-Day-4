ec52296070ecbb52e6be1c21818ec89730e13440
MUTINTA <JustinMutinta@hotmail.com>
2021-08-27 12:23:20 -0500

    4th pass, random and lists

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-states_of_america = ["Delaware", "Pennsylvania", "Texas", "Oklahoma", "New York", "Arkansas"]
+import random
 
-print(states_of_america)
+str_inp = "Hello, from Python"
+names = str_inp.split(",")
 
-print(states_of_america[3])
+print(names)
 
-states_of_america.append("California")
+names_string = input("Give me everybody's names, separated by a comma. \n")
+names_list = names_string.split(", ")
 
-print(states_of_america)
+number = random.randint(0, len(names_list) - 1)                 #need the -1 because list's start at 0 and the len starts at 1
+print(names_list)
+print(f"The name chosen at random was {names_list[number]}")
 
-states_of_america.extend(["Georgia", "Montana", "Wisconsin", "Florida"])
-
-print(states_of_america)
+random_name = random.choice(names_list)                         #easier way to get random value from name_list
+print(random_name)
